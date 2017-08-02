91c4376011bbdcdcb21a7a44460817d4fbd95163
barry HEZAM <barry.hezam@hotmail.fr>
2017-08-02 18:37:26 +0200

    Création de la nouvelle version du  WindowsForm "AjouterCommandeInfoClient".

---
+++ WindowsFormsProjetL3MIAGE/WindowsFormsProjetL3MIAGE/IHM/AjouterCommande.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace WindowsFormsProjetL3MIAGE.IHM
+{
+    partial class AjouterCommandeInfoClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitreAco = new System.Windows.Forms.Label();
+            this.labelRechNomCli = new System.Windows.Forms.Label();
+            this.labelChoiClient = new System.Windows.Forms.Label();
+            this.textBoxRechNomCli = new System.Windows.Forms.TextBox();
+            this.comboBoxChoiCli = new System.Windows.Forms.ComboBox();
+            this.labelInfCli = new System.Windows.Forms.Label();
+            this.labelTelAco = new System.Windows.Forms.Label();
+            this.labelVilleAco = new System.Windows.Forms.Label();
+            this.labelAdrAco = new System.Windows.Forms.Label();
+            this.labelMailAco = new System.Windows.Forms.Label();
+            this.labelCpAco = new System.Windows.Forms.Label();
+            this.textBoxTelAco = new System.Windows.Forms.TextBox();
+            this.textBoxAdrAco = new System.Windows.Forms.TextBox();
+            this.textBoxMailAco = new System.Windows.Forms.TextBox();
+            this.textBoxCpAco = new System.Windows.Forms.TextBox();
+            this.textBoxVilleAco = new System.Windows.Forms.TextBox();
+            this.co = new System.Windows.Forms.Button();
+            this.buttonSuiAco = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTitreAco
+            // 
+            this.labelTitreAco.AutoSize = true;
+            this.labelTitreAco.Location = new System.Drawing.Point(12, 9);
+            this.labelTitreAco.Name = "labelTitreAco";
+            this.labelTitreAco.Size = new System.Drawing.Size(161, 13);
+            this.labelTitreAco.TabIndex = 0;
+            this.labelTitreAco.Text = "Enregistrement d\'une commande";
+            // 
+            // labelRechNomCli
+            // 
+            this.labelRechNomCli.AutoSize = true;
+            this.labelRechNomCli.Location = new System.Drawing.Point(12, 38);
+            this.labelRechNomCli.Name = "labelRechNomCli";
+            this.labelRechNomCli.Size = new System.Drawing.Size(154, 13);
+            this.labelRechNomCli.TabIndex = 1;
+            this.labelRechNomCli.Text = "Rechercher le nom d\'un client :";
+            // 
+            // labelChoiClient
+            // 
+            this.labelChoiClient.AutoSize = true;
+            this.labelChoiClient.Location = new System.Drawing.Point(12, 65);
+            this.labelChoiClient.Name = "labelChoiClient";
+            this.labelChoiClient.Size = new System.Drawing.Size(139, 13);
+            this.labelChoiClient.TabIndex = 2;
+            this.labelChoiClient.Text = "Choisir le numéro du client : ";
+            // 
+            // textBoxRechNomCli
+            // 
+            this.textBoxRechNomCli.Location = new System.Drawing.Point(200, 38);
+            this.textBoxRechNomCli.Name = "textBoxRechNomCli";
+            this.textBoxRechNomCli.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRechNomCli.TabIndex = 3;
+            // 
+            // comboBoxChoiCli
+            // 
+            this.comboBoxChoiCli.FormattingEnabled = true;
+            this.comboBoxChoiCli.Location = new System.Drawing.Point(200, 65);
+            this.comboBoxChoiCli.Name = "comboBoxChoiCli";
+            this.comboBoxChoiCli.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxChoiCli.TabIndex = 4;
+            // 
+            // labelInfCli
+            // 
+            this.labelInfCli.AutoSize = true;
+            this.labelInfCli.Location = new System.Drawing.Point(15, 120);
+            this.labelInfCli.Name = "labelInfCli";
+            this.labelInfCli.Size = new System.Drawing.Size(120, 13);
+            this.labelInfCli.TabIndex = 5;
+            this.labelInfCli.Text = "Informations sur le client";
+            // 
+            // labelTelAco
+            // 
+            this.labelTelAco.AutoSize = true;
+            this.labelTelAco.Location = new System.Drawing.Point(15, 154);
+            this.labelTelAco.Name = "labelTelAco";
+            this.labelTelAco.Size = new System.Drawing.Size(64, 13);
+            this.labelTelAco.TabIndex = 6;
+            this.labelTelAco.Text = "Téléphone :";
+            // 
+            // labelVilleAco
+            // 
+            this.labelVilleAco.AutoSize = true;
+            this.labelVilleAco.Location = new System.Drawing.Point(15, 285);
+            this.labelVilleAco.Name = "labelVilleAco";
+            this.labelVilleAco.Size = new System.Drawing.Size(26, 13);
+            this.labelVilleAco.TabIndex = 7;
+            this.labelVilleAco.Text = "Ville";
+            // 
+            // labelAdrAco
+            // 
+            this.labelAdrAco.AutoSize = true;
+            this.labelAdrAco.Location = new System.Drawing.Point(15, 256);
+            this.labelAdrAco.Name = "labelAdrAco";
+            this.labelAdrAco.Size = new System.Drawing.Size(54, 13);
+            this.labelAdrAco.TabIndex = 8;
+            this.labelAdrAco.Text = "Adresse : ";
+            // 
+            // labelMailAco
+            // 
+            this.labelMailAco.AutoSize = true;
+            this.labelMailAco.Location = new System.Drawing.Point(15, 214);
+            this.labelMailAco.Name = "labelMailAco";
+            this.labelMailAco.Size = new System.Drawing.Size(75, 13);
+            this.labelMailAco.TabIndex = 9;
+            this.labelMailAco.Text = "Adresse mail : ";
+            // 
+            // labelCpAco
+            // 
+            this.labelCpAco.AutoSize = true;
+            this.labelCpAco.Location = new System.Drawing.Point(15, 183);
+            this.labelCpAco.Name = "labelCpAco";
+            this.labelCpAco.Size = new System.Drawing.Size(73, 13);
+            this.labelCpAco.TabIndex = 10;
+            this.labelCpAco.Text = "Code Postal : ";
+            // 
+            // textBoxTelAco
+            // 
+            this.textBoxTelAco.Location = new System.Drawing.Point(200, 146);
+            this.textBoxTelAco.Name = "textBoxTelAco";
+            this.textBoxTelAco.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTelAco.TabIndex = 11;
+            // 
+            // textBoxAdrAco
+            // 
+            this.textBoxAdrAco.Location = new System.Drawing.Point(200, 253);
+            this.textBoxAdrAco.Name = "textBoxAdrAco";
+            this.textBoxAdrAco.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAdrAco.TabIndex = 12;
+            // 
+            // textBoxMailAco
+            // 
+            this.textBoxMailAco.Location = new System.Drawing.Point(200, 214);
+            this.textBoxMailAco.Name = "textBoxMailAco";
+            this.textBoxMailAco.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMailAco.TabIndex = 13;
+            // 
+            // textBoxCpAco
+            // 
+            this.textBoxCpAco.Location = new System.Drawing.Point(200, 176);
+            this.textBoxCpAco.Name = "textBoxCpAco";
+            this.textBoxCpAco.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCpAco.TabIndex = 14;
+            // 
+            // textBoxVilleAco
+            // 
+            this.textBoxVilleAco.Location = new System.Drawing.Point(200, 285);
+            this.textBoxVilleAco.Name = "textBoxVilleAco";
+            this.textBoxVilleAco.Size = new System.Drawing.Size(100, 20);
+            this.textBoxVilleAco.TabIndex = 15;
+            // 
+            // co
+            // 
+            this.co.Location = new System.Drawing.Point(18, 327);
+            this.co.Name = "co";
+            this.co.Size = new System.Drawing.Size(75, 23);
+            this.co.TabIndex = 16;
+            this.co.Text = "Annuler";
+            this.co.UseVisualStyleBackColor = true;
+            // 
+            // buttonSuiAco
+            // 
+            this.buttonSuiAco.Location = new System.Drawing.Point(225, 327);
+            this.buttonSuiAco.Name = "buttonSuiAco";
+            this.buttonSuiAco.Size = new System.Drawing.Size(75, 23);
+            this.buttonSuiAco.TabIndex = 17;
+            this.buttonSuiAco.Text = "Suivant";
+            this.buttonSuiAco.UseVisualStyleBackColor = true;
+            // 
+            // AjouterCommandeInfoClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 362);
+            this.Controls.Add(this.buttonSuiAco);
+            this.Controls.Add(this.co);
+            this.Controls.Add(this.textBoxVilleAco);
+            this.Controls.Add(this.textBoxCpAco);
+            this.Controls.Add(this.textBoxMailAco);
+            this.Controls.Add(this.textBoxAdrAco);
+            this.Controls.Add(this.textBoxTelAco);
+            this.Controls.Add(this.labelCpAco);
+            this.Controls.Add(this.labelMailAco);
+            this.Controls.Add(this.labelAdrAco);
+            this.Controls.Add(this.labelVilleAco);
+            this.Controls.Add(this.labelTelAco);
+            this.Controls.Add(this.labelInfCli);
+            this.Controls.Add(this.comboBoxChoiCli);
+            this.Controls.Add(this.textBoxRechNomCli);
+            this.Controls.Add(this.labelChoiClient);
+            this.Controls.Add(this.labelRechNomCli);
+            this.Controls.Add(this.labelTitreAco);
+            this.Name = "AjouterCommandeInfoClient";
+            this.Text = "AjouterCommande";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitreAco;
+        private System.Windows.Forms.Label labelRechNomCli;
+        private System.Windows.Forms.Label labelChoiClient;
+        private System.Windows.Forms.TextBox textBoxRechNomCli;
+        private System.Windows.Forms.ComboBox comboBoxChoiCli;
+        private System.Windows.Forms.Label labelInfCli;
+        private System.Windows.Forms.Label labelTelAco;
+        private System.Windows.Forms.Label labelVilleAco;
+        private System.Windows.Forms.Label labelAdrAco;
+        private System.Windows.Forms.Label labelMailAco;
+        private System.Windows.Forms.Label labelCpAco;
+        private System.Windows.Forms.TextBox textBoxTelAco;
+        private System.Windows.Forms.TextBox textBoxAdrAco;
+        private System.Windows.Forms.TextBox textBoxMailAco;
+        private System.Windows.Forms.TextBox textBoxCpAco;
+        private System.Windows.Forms.TextBox textBoxVilleAco;
+        private System.Windows.Forms.Button co;
+        private System.Windows.Forms.Button buttonSuiAco;
+    }
+}
