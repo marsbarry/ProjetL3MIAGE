8645d9cf43d5c7961f6bfc2bdc4493ec94d4c61b
barry HEZAM <barry.hezam@hotmail.fr>
2017-08-04 17:14:58 +0200

    Création de laclasse "GestionCommandeC".

---
+++ WindowsFormsProjetL3MIAGE/WindowsFormsProjetL3MIAGE/CLasse/GestionCommandeC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace WindowsFormsProjetL3MIAGE.CLasse
+{
+    class GestionCommandeC
+    {
+        public static int MaxCmd()
+        {
+            int nbmax;
+            string req = "Select MAX(numCmd) from Commande";
+            ConnexionBD uneReq = new ConnexionBD(req);
+            DataTable DtCmdMax = new DataTable();
+            DtCmdMax = uneReq.ExecuteSelect();
+            nbmax = Convert.ToInt32(DtCmdMax.Rows[0][0]);
+            nbmax = nbmax + 1;
+            return nbmax;
+        }
+    }
+}
