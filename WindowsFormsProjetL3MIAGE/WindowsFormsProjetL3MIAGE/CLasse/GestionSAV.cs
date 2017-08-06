e033453c179d046cfbac97da322fda4d05f6f26e
barry HEZAM <barry.hezam@hotmail.fr>
2017-08-06 16:21:42 +0200

    Création de la classe "GestionSAV".

---
+++ WindowsFormsProjetL3MIAGE/WindowsFormsProjetL3MIAGE/CLasse/GestionSAV.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace WindowsFormsProjetL3MIAGE.CLasse
+{
+    class GestionSAV
+    {
+        public int GetMAxId()
+        {
+            string req = " SELECT MAX(idlit) from litige";
+            DataTable dtMaxId;
+            ConnexionBD ObjMaxId = new ConnexionBD(req);
+            dtMaxId = ObjMaxId.ExecuteSelect();
+
+            int MaxId = Convert.ToInt32(dtMaxId.Rows[0]["MAX(idlit)"]);
+            MaxId = MaxId + 1;
+            return MaxId;
+        }
+
+    }
+}
